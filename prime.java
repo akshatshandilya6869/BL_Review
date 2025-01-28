da79c020a5bdf158d43f19ccdb3c2c8a1c941c8a
akshatshandilya6869 <akshatshandilya203@gmail.com>
2025-01-28 12:07:18 +0530

    Prime.java

---
+++ prime.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Prime{
+bool isPrime(int n){
+if(n==1 || n==0){
+return false;
+}
+for(int i=2;i<=n/2;i++){
+if(n%i==0){
+return false;
+}
+}
+}
+public static void main(String []args){
+int n;
+int n=i;
+bool ans=isPrime(i);
+for(int i=1;i<=100;i++){
+if(){
+System.out.println(i);
+}
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
